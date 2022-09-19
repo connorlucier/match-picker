d27d1ca7f180ca261c3b9770b562d1bd7b3dbbb0
Connor Lucier <connor@connorlucier.com>
2022-09-19 15:36:14 -0400

    add delete league/team functionality
    + add teams to league model

--- cmd/api/teams.go
+++ cmd/api/teams.go
@@ -8,13 +8,15 @@ import (
 )
 
 func getTeamsByLeague(c *gin.Context) {
+	id := c.Param("leagueId")
+
 	// TODO pagination
 	var teams []model.Team
-	db.Find(&teams)
+	db.Find(&teams, "league_id = ?", id)
 	c.JSON(http.StatusOK, teams)
 }
 
-func getTeamInLeague(c *gin.Context) {
+func getTeamById(c *gin.Context) {
 	var team model.Team
 	id := c.Param("teamId")
 	db.Limit(1).Find(&team, "id = ?", id)
@@ -38,4 +40,10 @@ func createTeam(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, team)
+}
+
+func deleteTeamById(c *gin.Context) {
+	id := c.Param("teamId")
+	db.Delete(&model.Team{}, "id = ?", id)
+	c.Status(http.StatusNoContent)
 }
